a113af4d845d8dba6d35532f8ca7046795572414
SaraCohen3446 <shhryk0659@gmail.com>
2025-12-07 13:48:52 +0200

    Fetch garages from government API and return results via API

---
+++ backend/CarWorkshopAPI/Controllers/GarageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CarWorkshopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GarageController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public GarageController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet("fetch")]
+        public async Task<IActionResult> FetchGarages()
+        {
+            var url = "https://data.gov.il/api/3/action/datastore_search?resource_id=bb68386a-a331-4bbc-b668-bba2766d517d&limit=5";
+
+            var response = await _httpClient.GetStringAsync(url);
+
+            return Ok(response);
+        }
+    }
+}
